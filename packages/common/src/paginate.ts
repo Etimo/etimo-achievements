66662840ece42260c4e38ad164fec8ec022f4049
Niclas Lindstedt <nicl@slindstedt.se>
2021-10-22 08:29:02 +0200

    fix: improve pagination object

--- packages/common/src/paginate.ts
+++ packages/common/src/paginate.ts
@@ -1,20 +1,26 @@
-export type PaginationType<T> = {
-  totalItems: number;
+export type PageInfoType = {
   items: number;
+  totalItems: number;
   itemsPerPage: number;
   currentPage: number;
   totalPages: number;
+};
+
+export type PaginationType<T> = {
   data: T[];
+  pagination: PageInfoType;
 };
 
 export function paginate<T>(data: T[], skip: number, take: number, count: number): PaginationType<T> {
   const currentPage = skip === 0 ? 1 : Math.floor(skip / take) + 1;
   return {
-    totalItems: count,
-    items: data.length,
-    itemsPerPage: take,
-    currentPage,
-    totalPages: Math.ceil(count / take),
     data: data,
+    pagination: {
+      items: data.length,
+      totalItems: count,
+      itemsPerPage: take,
+      currentPage,
+      totalPages: Math.ceil(count / take),
+    },
   };
 }
