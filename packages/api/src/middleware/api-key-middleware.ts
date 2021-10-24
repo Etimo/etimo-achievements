9984bc03282f12f627dff3e93a818d1293de6843
Niclas Lindstedt <nicl@slindstedt.se>
2021-10-24 20:21:32 +0200

    chore: add version peek script

--- packages/api/src/middleware/api-key-middleware.ts
+++ packages/api/src/middleware/api-key-middleware.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { UnauthorizedError } from '../errors/unauthorized-error';
 
+const excludedPaths = ['/version'];
+
 export const apiKeyMiddleware = () => {
   return (req: Request, res: Response, next: NextFunction) => {
+    if (excludedPaths.some((p) => req.path === p || req.path.startsWith(`${p}/`))) {
+      console.log(`Path ${req.path} is excluded from authentication`);
+      return next();
+    }
     if (req.headers['x-api-key'] !== process.env.API_KEY) {
       throw new UnauthorizedError('Invalid API key');
     }
