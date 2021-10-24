44d5a9f0463951d6a003f5c8224593c8e7b7dff8
Niclas Lindstedt <nicl@slindstedt.se>
2021-10-24 17:27:14 +0200

    fix: improve error handling

--- packages/api/src/middleware/error-middleware.ts
+++ packages/api/src/middleware/error-middleware.ts
@@ -1,5 +1,6 @@
 import {
   CheckViolationError,
+  ConstraintViolationError,
   DataError,
   DBError,
   ForeignKeyViolationError,
@@ -9,6 +10,9 @@ import {
   ValidationError,
 } from '@etimo-achievements/service';
 import { NextFunction, Request, Response } from 'express';
+import { BadRequestError } from '../errors/bad-request-error';
+import { ForbiddenError } from '../errors/forbidden-error';
+import { UnauthorizedError } from '../errors/unauthorized-error';
 import { ValidationError as ApiValidationError } from '../errors/validation-error';
 
 export const errorMiddleware = () => {
@@ -18,122 +22,144 @@ export const errorMiddleware = () => {
 };
 
 export function handleError(error: any, res: Response) {
-  if (error instanceof ApiValidationError) {
-    return res.status(400).send(error.message);
+  switch (error.constructor) {
+    case ValidationError:
+      return handleValidationError(error, res);
+
+    case ApiValidationError:
+    case BadRequestError:
+      return res.status(400).send(error.message);
+
+    case UnauthorizedError:
+      return res.status(401).send(error.message);
+
+    case ForbiddenError:
+      return res.status(403).send(error.message);
+
+    default:
+      return handleRemainingErrors(error, res);
   }
+}
 
-  if (error instanceof ValidationError) {
-    switch (error.type) {
-      case 'ModelValidation':
-        return res.status(400).send({
-          message: error.message,
-          type: error.type,
-          data: error.data,
-        });
+function handleValidationError(error: any, res: Response) {
+  switch (error.type) {
+    case 'ModelValidation':
+      return res.status(400).send({
+        message: error.message,
+        type: error.type,
+        data: error.data,
+      });
 
-      case 'RelationExpression':
-        return res.status(400).send({
-          message: error.message,
-          type: 'RelationExpression',
-          data: {},
-        });
+    case 'RelationExpression':
+      return res.status(400).send({
+        message: error.message,
+        type: 'RelationExpression',
+        data: {},
+      });
 
-      case 'UnallowedRelation':
-        return res.status(400).send({
-          message: error.message,
-          type: error.type,
-          data: {},
-        });
+    case 'UnallowedRelation':
+      return res.status(400).send({
+        message: error.message,
+        type: error.type,
+        data: {},
+      });
 
-      case 'InvalidGraph':
-        return res.status(400).send({
-          message: error.message,
-          type: error.type,
-          data: {},
-        });
+    case 'InvalidGraph':
+      return res.status(400).send({
+        message: error.message,
+        type: error.type,
+        data: {},
+      });
 
-      default:
-        return res.status(400).send({
-          message: error.message,
-          type: 'UnknownValidationError',
-          data: {},
-        });
-    }
+    default:
+      return res.status(400).send({
+        message: error.message,
+        type: 'UnknownValidationError',
+        data: {},
+      });
   }
+}
 
-  if (error instanceof NotFoundError) {
-    return res.status(404).send({
-      message: error.message,
-      type: 'NotFound',
-      data: {},
-    });
+function handleRemainingErrors(error: any, res: Response) {
+  switch (error.constructor) {
+    case NotFoundError:
+      return res.status(404).send({
+        message: error.message,
+        type: 'NotFound',
+        data: {},
+      });
+
+    case UniqueViolationError:
+      return res.status(409).send({
+        message: error.message,
+        type: 'UniqueViolation',
+        data: {
+          columns: error.columns,
+          table: error.table,
+          constraint: error.constraint,
+        },
+      });
+
+    case NotNullViolationError:
+      return res.status(400).send({
+        message: error.message,
+        type: 'NotNullViolation',
+        data: {
+          column: error.column,
+          table: error.table,
+        },
+      });
+
+    case ForeignKeyViolationError:
+      return res.status(409).send({
+        message: error.message,
+        type: 'ForeignKeyViolation',
+        data: {
+          table: error.table,
+          constraint: error.constraint,
+        },
+      });
+
+    case ConstraintViolationError:
+      return res.status(409).send({
+        message: error.message,
+        type: 'ConstraintViolation',
+        data: {
+          table: error.table,
+          constraint: error.constraint,
+        },
+      });
+
+    case CheckViolationError:
+      return res.status(400).send({
+        message: error.message,
+        type: 'CheckViolation',
+        data: {
+          table: error.table,
+          constraint: error.constraint,
+        },
+      });
+
+    case DataError:
+      return res.status(400).send({
+        message: error.message,
+        type: 'InvalidData',
+        data: {},
+      });
+
+    case DBError:
+      return res.status(500).send({
+        message: error.message,
+        type: 'UnknownDatabaseError',
+        data: {},
+      });
+
+    default:
+      console.error(error.stack);
+      return res.status(500).send({
+        message: error.message,
+        type: 'UnknownError',
+        data: {},
+      });
   }
-
-  if (error instanceof UniqueViolationError) {
-    return res.status(409).send({
-      message: error.message,
-      type: 'UniqueViolation',
-      data: {
-        columns: error.columns,
-        table: error.table,
-        constraint: error.constraint,
-      },
-    });
-  }
-
-  if (error instanceof NotNullViolationError) {
-    return res.status(400).send({
-      message: error.message,
-      type: 'NotNullViolation',
-      data: {
-        column: error.column,
-        table: error.table,
-      },
-    });
-  }
-
-  if (error instanceof ForeignKeyViolationError) {
-    return res.status(409).send({
-      message: error.message,
-      type: 'ForeignKeyViolation',
-      data: {
-        table: error.table,
-        constraint: error.constraint,
-      },
-    });
-  }
-
-  if (error instanceof CheckViolationError) {
-    return res.status(400).send({
-      message: error.message,
-      type: 'CheckViolation',
-      data: {
-        table: error.table,
-        constraint: error.constraint,
-      },
-    });
-  }
-
-  if (error instanceof DataError) {
-    return res.status(400).send({
-      message: error.message,
-      type: 'InvalidData',
-      data: {},
-    });
-  }
-
-  if (error instanceof DBError) {
-    return res.status(500).send({
-      message: error.message,
-      type: 'UnknownDatabaseError',
-      data: {},
-    });
-  }
-
-  console.error(error.stack);
-  return res.status(500).send({
-    message: error.message,
-    type: 'UnknownError',
-    data: {},
-  });
 }
