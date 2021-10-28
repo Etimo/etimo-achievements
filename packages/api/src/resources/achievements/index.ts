0c124fd13591335310c363a86a539f5d378454b7
Rickard Löfberg <rickard.lofberg@etimo.se>
2021-10-29 00:42:47 +0200

    fix: change filename achievement

--- packages/api/src/resources/achievements/index.ts
+++ packages/api/src/resources/achievements/index.ts
@@ -1,4 +1,4 @@
-export * from './acievements-controller';
-export * from './achievements-dto';
+export * from './achievement-controller';
+export * from './achievement-dto';
 export * from './achievement-mapper';
 export * from './achievement-validator';
