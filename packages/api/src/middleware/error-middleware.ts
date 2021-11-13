4dbcf916023e17e1c05e88b9f21589cc96459772
Niclas Lindstedt <nicl@slindstedt.se>
2021-11-13 16:47:47 +0100

    fix: send error status in error middleware

--- packages/api/src/middleware/error-middleware.ts
+++ packages/api/src/middleware/error-middleware.ts
@@ -10,15 +10,15 @@ export const errorMiddleware = () => {
 export function handleError(error: any, res: Response) {
   switch (error.constructor) {
     case UnauthorizedError:
-      return res.status(401);
+      return res.status(401).send();
 
     case NotFoundError:
-      return res.status(404);
+      return res.status(404).send();
 
     case ConflictError:
-      return res.status(409);
+      return res.status(409).send();
 
     default:
-      return res.status(500);
+      return res.status(500).send();
   }
 }
