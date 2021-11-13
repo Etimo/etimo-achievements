07fcf5ed34a4e6ade0b0d5b58060f8801b07e8b3
Niclas Lindstedt <nicl@slindstedt.se>
2021-11-13 20:52:17 +0100

    ci: wip

--- packages/data/seeds/00-users.d.ts
+++
@@ -1,3 +0,0 @@
-import * as Knex from 'knex';
-export declare function seed(knex: Knex): Promise<void>;
-//# sourceMappingURL=00-users.d.ts.map
--- packages/data/seeds/01-achievements.d.ts
+++
@@ -1,3 +0,0 @@
-import * as Knex from 'knex';
-export declare function seed(knex: Knex): Promise<void>;
-//# sourceMappingURL=01-achievements.d.ts.map
