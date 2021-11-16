594410d8410d7527b5de0ac0170fb9776087847d
Niclas Lindstedt <niclaslindstedt@users.noreply.github.com>
2021-11-16 19:04:25 +0100

    feat: add default colors to logger (#67)

--- packages/common/src/logger.ts
+++ packages/common/src/logger.ts
@@ -1,31 +1,93 @@
+type LoggerOptions = {
+  extras?: any[];
+  color?: LoggingColor;
+  timestamp?: boolean;
+};
+
+export enum LoggingColor {
+  Reset,
+  Bright,
+  Normal,
+  Dim,
+  Green,
+  Yellow,
+  Red,
+}
+
 export class Logger {
   private static instance?: Logger;
 
-  public static log(message: string, ...optionalParams: any[]) {
+  public static log(message: string, options?: LoggerOptions) {
     if (!Logger.instance) {
       Logger.instance = new Logger();
     }
 
-    Logger.instance.info(message, ...optionalParams);
+    Logger.instance.info(message, options);
   }
 
-  public trace(message: string, ...optionalParams: any[]) {
-    console.trace(timestamp() + message, ...optionalParams);
+  public trace(message: string, options?: LoggerOptions) {
+    this.output(message, LoggingColor.Dim, console.debug, options);
   }
 
-  public info(message: string, ...optionalParams: any[]) {
-    console.info(timestamp() + message, ...optionalParams);
+  public debug(message: string, options?: LoggerOptions) {
+    this.output(message, LoggingColor.Normal, console.debug, options);
   }
 
-  public warn(message: string, ...optionalParams: any[]) {
-    console.warn(timestamp() + message, ...optionalParams);
+  public info(message: string, options?: LoggerOptions) {
+    this.output(message, LoggingColor.Bright, console.info, options);
   }
 
-  public error(message: string, ...optionalParams: any[]) {
-    console.error(timestamp() + message, ...optionalParams);
+  public warn(message: string, options?: LoggerOptions) {
+    this.output(message, LoggingColor.Yellow, console.warn, options);
+  }
+
+  public error(message: string, options?: LoggerOptions) {
+    this.output(message, LoggingColor.Red, console.error, options);
+  }
+
+  private output(
+    message: string,
+    defaultColor: LoggingColor,
+    logFn: (message: string, ...args: any[]) => void,
+    options?: LoggerOptions
+  ) {
+    const output = this.getMessage(message, { color: defaultColor, ...options });
+    if (options?.extras) {
+      logFn(output, options.extras);
+    }
+    logFn(output);
+  }
+
+  private getMessage(message: string, options?: LoggerOptions) {
+    const reset = colorCode(LoggingColor.Reset);
+    const color = options?.color ? colorCode(options.color) : reset;
+    return `${color}${timestamp(options?.timestamp)}${message}${reset}`;
   }
 }
 
-function timestamp() {
-  return `[${new Date().toTimeString().split(' ')[0]}] `;
+function colorCode(color: LoggingColor): string {
+  switch (color) {
+    case LoggingColor.Bright:
+      return '';
+    case LoggingColor.Dim:
+      return '\x1b[2m';
+    case LoggingColor.Green:
+      return '\x1b[32m';
+    case LoggingColor.Yellow:
+      return '\x1b[33m';
+    case LoggingColor.Red:
+      return '\x1b[31m';
+    case LoggingColor.Reset:
+      return '\x1b[0m';
+    case LoggingColor.Normal:
+    default:
+      return '\x1b[90m';
+  }
+}
+
+function timestamp(enabled?: boolean) {
+  if (enabled === undefined || enabled) {
+    return `[${new Date().toTimeString().split(' ')[0]}] `;
+  }
+  return '';
 }
