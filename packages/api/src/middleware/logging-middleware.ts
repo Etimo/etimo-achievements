592cd7f4495efdf078d346baa0c0591cd97fbdc1
Niclas Lindstedt <nicl@slindstedt.se>
2021-11-16 17:16:41 +0100

    feat: add default colors to logger

--- packages/api/src/middleware/logging-middleware.ts
+++ packages/api/src/middleware/logging-middleware.ts
@@ -1,11 +1,8 @@
+import { LoggingColor } from '@etimo-achievements/common';
 import { NextFunction, Request, Response } from 'express';
 import { getContext } from '../utils';
 
 const ignoredPaths: string[] = ['/favicon.ico'];
-
-const reset = '\x1b[0m';
-const red = '\x1b[31m';
-const green = '\x1b[32m';
 
 export const loggingMiddleware = () => {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -14,22 +11,23 @@ export const loggingMiddleware = () => {
     }
 
     const ctx = getContext();
+    const count = ctx.requestCount;
+    const rid = ctx.shortRequestId;
 
-    ctx.logger.info(
-      `${green}[${ctx.timestamp}] [${ctx.requestCount}] -> ${req.method} ${req.path} {${ctx.shortRequestId}} [${req.ip}]${reset}`
-    );
+    let color = LoggingColor.Green;
+    ctx.logger.info(`[${count}] -> ${req.method} ${req.path} {${rid}} [${req.ip}]`, { color });
+
     next();
-    const color = getColor(res.statusCode);
-    ctx.logger.info(
-      `${color}[${ctx.timestamp}] [${ctx.requestCount}] <- ${res.statusCode} {${ctx.shortRequestId}}${reset}`
-    );
+
+    color = getColor(res.statusCode);
+    ctx.logger.info(`[${count}] <- ${res.statusCode} {${rid}}`, { color });
   };
 };
 
 const getColor = (statusCode?: number) => {
   if (!statusCode) {
-    return red;
+    return LoggingColor.Red;
   }
 
-  return statusCode >= 200 && statusCode <= 299 ? green : red;
+  return statusCode >= 200 && statusCode <= 299 ? LoggingColor.Green : LoggingColor.Red;
 };
