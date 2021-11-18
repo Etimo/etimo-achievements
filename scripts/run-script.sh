f5ede0410cbc136c338f493a4af5758185562aea
Niclas Lindstedt <nicl@slindstedt.se>
2021-11-18 21:03:43 +0100

    chore: list packages script

--- scripts/run-script.sh
+++ scripts/run-script.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-for package_path in packages/*; do
-  [ -d "$package_path" ] && {
-    (cd "$package_path" || exit 1
-    npm run "$*") || exit 1
-  }
-done
+main() {
+  packages=$("$_script_path/list-packages.sh")
+  for package in $packages; do
+    (cd "$_root_path/packages/$package" || exit 1
+    time -p npm run "$*") || exit 1
+  done
+}
+
+# Setup paths
+_script_path="$(dirname "$(readlink -f "$0")")"
+_root_path="$(readlink -f "$_script_path/..")"
+
+main "$*"
