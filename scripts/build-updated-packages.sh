b648a10b100960b610f1a698035331f83d451107
Niclas Lindstedt <nicl@slindstedt.se>
2021-11-21 21:24:32 +0100

    chore: finish script

--- scripts/build-updated-packages.sh
+++ scripts/build-updated-packages.sh
@@ -4,29 +4,28 @@ built_packages=()
 
 main() {
   for package_path in $(find "$_packages_path" -mindepth 1 -maxdepth 1 -type d); do
+    ! has_updated_files "$package_path" && continue
+    echo "Detected changes in $(basename "$package_path") -- building"
     build_package_tree "$package_path"
   done
 
 }
 
 build_package_tree() {
-  ! has_updated_files "$1" && return 0
-
   package_name=$(basename "$1")
   dependants=$(get_dependants "$package_name")
-  echo "Building $package_name + $dependants"
   build_package "$package_name"
   for p in ${dependants[*]}; do
-    build_package_tree "$p"
+    echo "Building dependant package $p"
+    build_package_tree "$_packages_path/$p"
   done
 }
 
 build_package() {
   [[ " ${built_packages[*]} " =~ [[:space:]]"$1"[[:space:]] ]] && return 0
 
-  echo "Building $1"
-  #(cd "$_packages_path/$1" || exit 1
-  #npm run compile) || exit 1
+  (cd "$_packages_path/$1" || exit 1
+  npm run compile) || exit 1
 
   built_packages+=("$1")
   date +%Y-%m-%dT%H:%M:%S > "$_packages_path/$1/.latest_build"
@@ -35,22 +34,21 @@ build_package() {
 has_updated_files() {
   latest_build_file="$1/.latest_build"
 
-  [ ! -f "$latest_build_file" ] && echo "1970-01-01T00:00:00" > "$latest_build_file"
+  [ ! -f "$latest_build_file" ] && {
+    echo "1970-01-01T00:00:00" > "$latest_build_file"
+  }
 
   package_mdate=$(cat "$latest_build_file")
 
-  (cd "$_packages_path" || exit 1
-  find "." -type f -newermt "$package_mdate" \
-    -iname "*.ts" \
-    -or -iname "*.tsx" \
-    -or -iname "*.js" \
-    -or -iname "*.json" \
+  mfiles=$(find "$1" -type f -newermt "$package_mdate" \
+    -not -name ".*" \
     -not -name "*.d.ts" \
     -not -path "*/dist/*" \
     -not -path "*/build/*" \
     -not -path "*/node_modules/*" \
-    -not -path "*/migrations/*" \
-    | sed -r "s/^.\///" | cut -f1 -d/ | sort -u)
+    -not -path "*/migrations/*" | wc -l)
+
+  (( mfiles ))
 }
 
 get_dependants() {
