3d891c64b7daca7822f0cfb2de68eedf24dd5819
Niclas Lindstedt <nicl@slindstedt.se>
2021-11-23 21:46:30 +0100

    docs: add examples to all dtos

--- packages/api/src/resources/slack/slack-controller.ts
+++ packages/api/src/resources/slack/slack-controller.ts
@@ -1,16 +1,20 @@
-import { ShowSlackAchievementsService } from '@etimo-achievements/service';
+import { CreateSlackAchievementsService, ShowSlackAchievementsService } from '@etimo-achievements/service';
 import { Request, Response, Router } from 'express';
 import { endpoint } from '../../utils';
 
 export type SlackControllerOptions = {
   showSlackAchievementsService?: ShowSlackAchievementsService;
+  createSlackAchievementsService?: CreateSlackAchievementsService;
 };
 
 export class SlackController {
   private showSlackAchievementsService: ShowSlackAchievementsService;
+  private createSlackAchievementsService: CreateSlackAchievementsService;
 
   constructor(options?: SlackControllerOptions) {
     this.showSlackAchievementsService = options?.showSlackAchievementsService ?? new ShowSlackAchievementsService();
+    this.createSlackAchievementsService =
+      options?.createSlackAchievementsService ?? new CreateSlackAchievementsService();
   }
 
   public get routes(): Router {
@@ -20,7 +24,7 @@ export class SlackController {
      * @openapi
      * /slack/achievements:
      *   get:
-     *     summary: Show Slack achievements modal
+     *     summary: Display achievement list modal in Slack
      *     security:
      *       - ApiKeyHeader: []
      *       - ApiKeyParameter: []
@@ -29,17 +33,41 @@ export class SlackController {
      *       - *takeParam
      *     responses:
      *       200:
-     *         description: Achievements modal was created.
+     *         description: The modal was created.
      *     tags:
      *       - Slack
      */
     router.get('/slack/achievements', endpoint(this.getAchievements));
 
+    /**
+     * @openapi
+     * /slack/create-achievement:
+     *   post:
+     *     summary: Display achievement creation modal in Slack
+     *     security:
+     *       - ApiKeyHeader: []
+     *       - ApiKeyParameter: []
+     *     responses:
+     *       200:
+     *         description: The modal was created.
+     *       400:
+     *         description: Request contains a missing or invalid argument.
+     *     tags:
+     *       - Slack
+     */
+    router.post('/slack/create-achievement', endpoint(this.displayAchievementModal));
+
     return router;
   }
 
-  private getAchievements = async (req: Request, res: Response) => {
+  private getAchievements = async (_req: Request, res: Response) => {
     await this.showSlackAchievementsService.show();
+
+    return res.status(200).send();
+  };
+
+  private displayAchievementModal = async (req: Request, res: Response) => {
+    await this.createSlackAchievementsService.getModal(req.body.trigger_id);
 
     return res.status(200).send();
   };
