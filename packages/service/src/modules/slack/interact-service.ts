634de846168e318fb09e380303a71a4e8fa6853b
Morgan Cromell <morgan.cromell@etimo.se>
2021-12-13 08:49:34 +0100

    Improved award message

--- packages/service/src/modules/slack/interact-service.ts
+++ packages/service/src/modules/slack/interact-service.ts
@@ -1,6 +1,6 @@
 import { Logger } from '@etimo-achievements/common';
-import { AchievementRepository } from '@etimo-achievements/data';
-import { WebClient } from '@slack/web-api';
+import { AchievementRepository, AwardRepository } from '@etimo-achievements/data';
+import { Block, ChatPostMessageArguments, ImageBlock, KnownBlock, WebClient } from '@slack/web-api';
 import { ServiceOptions } from '..';
 
 interface ActionByCallbackId {
@@ -9,11 +9,12 @@ interface ActionByCallbackId {
 
 export class SlackInteractService {
   private achievementRepo: AchievementRepository;
+  private awardRepo: AwardRepository;
   private web: WebClient;
 
   constructor(options?: ServiceOptions) {
     this.achievementRepo = options?.achievementRepository ?? new AchievementRepository();
-
+    this.awardRepo = options?.awardRepository ?? new AwardRepository();
     const token = process.env.SLACK_TOKEN;
     this.web = new WebClient(token);
   }
@@ -24,22 +25,60 @@ export class SlackInteractService {
   }
 
   private async awardAchievement(payload: any) {
+    Logger.log(JSON.stringify(payload));
     const fromUser = payload.user;
     const metadata = JSON.parse(payload.view.private_metadata);
     const channel = metadata.channelId;
     const values = payload.view.state.values;
-    const achievement = values['achievement-input']['static_select-action'].selected_option.text.text;
+    const achievementId = values['achievement-input']['static_select-action'].selected_option.value;
     const toUsers = values['user-input']['multi_users_select-action'].selected_users;
+
+    const achievement = await this.achievementRepo.findById(achievementId);
 
     const usersMessage = this.joinStrings(toUsers.map((user: string) => `<@${user}>`));
 
-    const message = `<@${fromUser.id}> Gave ${usersMessage} the achievement: ${achievement}`;
-    Logger.log(message);
+    const messageText = ` <@${fromUser.id}> Gave ${usersMessage} an award!`;
 
-    await this.web.chat.postMessage({
+    const selfMention = toUsers.length == 1 && toUsers[0] == fromUser.id;
+
+    const message: ChatPostMessageArguments = {
       channel: channel,
-      text: message,
-    });
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: messageText,
+          },
+        },
+        {
+          type: 'divider',
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*${achievement.name}*\n${achievement.description}`,
+          },
+          accessory: {
+            type: 'image',
+            image_url: selfMention
+              ? 'https://i.kym-cdn.com/entries/icons/original/000/030/329/cover1.jpg'
+              : 'https://ca.slack-edge.com/T044B4VDU-U01LKS59T1Q-4bd995b5e3e0-512',
+            alt_text: 'medal',
+          },
+        },
+      ],
+    };
+
+    // for (var user in toUsers) {
+    //   await this.awardRepo.create({
+    //     achievementId: achievementId,
+    //     userId: user,
+    //   });
+    // }
+
+    await this.web.chat.postMessage(message);
   }
 
   private actions: ActionByCallbackId = {
