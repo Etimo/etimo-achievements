3530af43a2c1b73a726c1077bd8843d79605f68e
Niclas Lindstedt <nicl@slindstedt.se>
2022-01-04 11:53:13 +0100

    refactor some scripts

--- scripts/run-script.js
+++ scripts/run-script.js
@@ -1,17 +1,4 @@
-import getBuildOrder from './utils/get-build-order.js';
-import { getPackageDirectory } from './utils/path-helper.js';
-import runCommand from './utils/run-command.js';
-
-export default async function runScript(script, params) {
-  const packages = getBuildOrder();
-  for (const packageName of packages) {
-    const cwd = getPackageDirectory(packageName);
-    const paramsStr = params.length ? ` ${params.join(' ')}` : '';
-    console.log(`Running 'npm run ${script}${paramsStr}' on ${packageName}...`)
-    const success = await runCommand('npm', ['run', script, ...params], cwd);
-    if (!success) { process.exit(1); }
-  }
-}
+import runScript from `./utils/run-script.js`;
 
 const script = process.argv[2];
 const params = process.argv.slice(3);
---
+++ scripts/utils/run-script.js
@@ -0,0 +1,14 @@
+import getBuildOrder from './utils/get-build-order.js';
+import { getPackageDirectory } from './utils/path-helper.js';
+import runCommand from './utils/run-command.js';
+
+export default async function runScript(script, params) {
+  const packages = getBuildOrder();
+  for (const packageName of packages) {
+    const cwd = getPackageDirectory(packageName);
+    const paramsStr = params.length ? ` ${params.join(' ')}` : '';
+    console.log(`Running 'npm run ${script}${paramsStr}' on ${packageName}...`)
+    const success = await runCommand('npm', ['run', script, ...params], cwd);
+    if (!success) { process.exit(1); }
+  }
+}
