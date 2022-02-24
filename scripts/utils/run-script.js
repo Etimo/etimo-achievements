02abdfa45e0e273fd6e7ece060f98c0302ee142d
Niclas Lindstedt <niclaslindstedt@users.noreply.github.com>
2022-02-24 15:45:21 +0100

    fix: run prettier on generated files after generation (#124)

--- scripts/utils/run-script.js
+++ scripts/utils/run-script.js
@@ -6,7 +6,8 @@ export default async function runScript(script, params) {
   const packages = getBuildOrder();
   for (const packageName of packages) {
     const cwd = getPackageDirectory(packageName);
-    const paramsStr = params.length ? ` ${params.join(' ')}` : '';
+    params = params || [];
+    const paramsStr = params.length > 0 ? ` ${params.join(' ')}` : '';
     console.log(`Running 'npm run ${script}${paramsStr}' on ${packageName}...`);
     const success = await runCommand('npm', ['run', script, ...params], cwd);
     if (!success) {
