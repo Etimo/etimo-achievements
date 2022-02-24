dc6491716b34f12dea41a230feaa0602a1c7bbee
Julius Willén <69385664+JuliusWillen@users.noreply.github.com>
2022-02-24 13:00:58 +0100

    feat: admin frontend (#119)

--- scripts/copy-env.js
+++ scripts/copy-env.js
@@ -1,6 +1,18 @@
-import { copyNoClobber } from './utils/file-helper.js';
+import { copy, copyNoClobber } from './utils/file-helper.js';
 
-const from = process.argv[2];
-const to = process.argv[3];
+const paths = [
+  ['packages/api/.env.defaults', 'packages/api/.env'],
+  ['packages/web/.env.defaults', 'packages/web/.env'],
+];
 
-copyNoClobber(from, to);
+if (process.argv.includes('--force')) {
+  copyFiles(copy);
+} else {
+  copyFiles(copyNoClobber);
+}
+
+function copyFiles(copyFn) {
+  for (const path of paths) {
+    copyFn(path[0], path[1]);
+  }
+}
