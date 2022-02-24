83fa298e8af9510e28e70a99a73ae1a3d9b4afe1
Niclas Lindstedt <niclaslindstedt@users.noreply.github.com>
2022-02-24 15:45:21 +0100

    Revert "fix: run prettier on generated files after generation (#124)"
    This reverts commit 02abdfa45e0e273fd6e7ece060f98c0302ee142d.

--- scripts/update-references.js
+++ scripts/update-references.js
@@ -1,5 +1,3 @@
-import runCommand from './utils/run-command.js';
 import updateReferences from './utils/update-references.js';
 
 updateReferences('@etimo-achievements');
-runCommand('npm', ['run', 'prettier-configs']);
