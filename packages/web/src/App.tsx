47791253149a5a1f18591e92c32f81c2380973ed
JuliusWillen <juliuswillen@gmail.com>
2022-02-24 11:05:18 +0100

    feat: react app

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.dawn.com/primary/2014/07/53bb2b6dad071.jpg" className="App-logo" alt="logo" />
+        <p>Hello Etimo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
