ba7fa2fe8de1373d43bf89ad95ed660014a47026
Niclas Lindstedt <niclaslindstedt@users.noreply.github.com>
2022-02-24 09:27:09 +0100

    Update file-helper.js

--- scripts/utils/file-helper.js
+++ scripts/utils/file-helper.js
@@ -2,7 +2,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { getRootDirectory } from './path-helper.js';
 
-
 export function loadFileAsObject(filePath) {
   const pathWithSlash = path.isAbsolute(filePath) ? filePath : `${getRootDirectory()}/${filePath}`;
   return JSON.parse(fs.readFileSync(pathWithSlash));
