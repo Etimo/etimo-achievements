75198235755c78ac682a227f8807dc73621661e4
Julius Willén <69385664+JuliusWillen@users.noreply.github.com>
2022-02-25 12:10:23 +0100

    feat: react router and public views (#131)

---
+++ packages/web/src/views/public/HomePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Etimo</p>
+      </header>
+    </div>
+  );
+};
+
+export default HomePage;
