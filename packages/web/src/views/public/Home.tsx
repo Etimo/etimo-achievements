3b1d8d50c1ec86f2f3a9dd70ac98a7a661bbb74b
Niclas Lindstedt <nicl@slindstedt.se>
2022-03-15 22:03:35 +0100

    refactor: use webpack instead of react-scripts for web

---
+++ packages/web/src/views/public/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Etimo</p>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
--- scripts/install-dependencies.js
+++ scripts/install-dependencies.js
@@ -1,5 +1,5 @@
 import installDependencies from './utils/install-dependencies.js';
 
-const wantedDependencies = ['jest', 'knex', 'ts-node', 'typescript:tsc', 'react-scripts'];
+const wantedDependencies = ['jest', 'knex', 'ts-node', 'typescript:tsc', 'webpack-cli:webpack'];
 
 installDependencies(wantedDependencies);
