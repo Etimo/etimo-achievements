4ad54c71b5542dd16f6ba082ccf3350fc48fe58f
Niclas Lindstedt <nicl@slindstedt.se>
2022-03-20 20:30:45 +0100

    chore: add tailwind and burger menu

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { slide as Menu } from 'react-burger-menu';
+import { Link, Route, Routes } from 'react-router-dom';
 import Home from './views/public/Home';
+import Login from './views/public/Login';
 
 const App = (): JSX.Element => {
   return (
     <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div>
+        <Menu>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+        </Menu>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </React.StrictMode>
   );
 };
--- packages/web/src/views/public/Home.tsx
+++ packages/web/src/views/public/Home.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Etimo</p>
-      </header>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
