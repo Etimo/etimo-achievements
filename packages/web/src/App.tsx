b2060595114b56666a2b63cfe5edd2f3af41e320
Niclas Lindstedt <nicl@slindstedt.se>
2022-03-22 20:11:54 +0100

    chore: wip

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './views/public/Home';
 import Login from './views/public/Login';
 
 const App = (): JSX.Element => {
   return (
     <React.StrictMode>
-      <div>
-        <Menu>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-        </Menu>
-      </div>
+      <SideMenu />
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
