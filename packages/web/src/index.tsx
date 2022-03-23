109ab15dffb7ca2e6005ef9ecafb74a2ded774b9
Niclas Lindstedt <nicl@slindstedt.se>
2022-03-23 22:15:18 +0100

    chore: remove cookie provider

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { CookiesProvider } from 'react-cookie';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -7,9 +6,7 @@ import './styles/index.css';
 
 const element = (
   <BrowserRouter>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
+    <App />
   </BrowserRouter>
 );
 
