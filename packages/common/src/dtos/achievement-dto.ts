c5717864682e5fcdd9fff69915ffe9adb85c579b
Niclas Lindstedt <nicl@slindstedt.se>
2022-04-10 11:21:47 +0200

    refactor: achievement cleanup

--- packages/common/src/dtos/achievement-dto.ts
+++ packages/common/src/dtos/achievement-dto.ts
@@ -11,10 +11,12 @@
  *         name:
  *           type: string
  *           description: The name of the achievement
+ *           maxLength: 255
  *           example: Coffee Achievement
  *         description:
  *           type: string
  *           description: A description of when the achievement should be awarded
+ *           maxLength: 255
  *           example: Put on coffee for the gang.
  *         achievementPoints:
  *           type: integer
