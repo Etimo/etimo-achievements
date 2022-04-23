a60a34f8d4df4cfb1e2f57971eefab1d37ecc05d
Niclas Lindstedt <nicl@slindstedt.se>
2022-04-23 16:52:19 +0200

    fix: return 403 on insufficient scope

--- packages/common/src/errors/index.ts
+++ packages/common/src/errors/index.ts
@@ -1,6 +1,7 @@
 export * from './bad-request-error';
 export * from './configuration-error';
 export * from './conflict-error';
+export * from './forbidden-error';
 export * from './internal-server-error';
 export * from './not-found-error';
 export * from './unauthorized-error';
