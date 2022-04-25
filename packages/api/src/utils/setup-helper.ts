eab6b6af959b24823580a8165ea7b5d1e04c3721
Niclas Lindstedt <nicl@slindstedt.se>
2022-04-25 20:46:24 +0200

    fix: improve logging further

--- packages/api/src/utils/setup-helper.ts
+++ packages/api/src/utils/setup-helper.ts
@@ -1,27 +1,26 @@
 import { isDevelopment, isLocal, isProduction } from '@etimo-achievements/common';
 import { Database } from '@etimo-achievements/data';
-import { Logger } from '@etimo-achievements/utils';
 import dotenv from 'dotenv';
 
 export function setupEnvironment(environment?: string) {
   let envFile = '.env';
   if (environment) {
     envFile += `.${environment}`;
-    Logger.log(`Using env file ${envFile}`);
+    console.log(`Using env file ${envFile}`);
   }
   dotenv.config({ path: `${__dirname}/../../${envFile}` });
 
-  Logger.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
   if (isLocal()) {
-    Logger.log(`API_KEY: ${process.env.API_KEY}`);
-    Logger.log('Local environment detected');
+    console.log(`API_KEY: ${process.env.API_KEY}`);
+    console.log('Local environment detected');
   }
   if (isDevelopment()) {
-    Logger.log('Development environment detected');
+    console.log('Development environment detected');
   }
   if (isProduction()) {
-    Logger.log('Production environment detected');
+    console.log('Production environment detected');
   }
 
   Database.connect();
--- packages/utils/src/logger/winston-logger.ts
+++ packages/utils/src/logger/winston-logger.ts
@@ -1,8 +1,6 @@
 import { ILogger, IRequestContext, LoggerOptions } from '@etimo-achievements/types';
 import winston from 'winston';
 
-type ILoggingContext = Pick<IRequestContext, 'requestId' | 'userId' | 'scopes'>;
-
 export class WinstonLogger implements ILogger {
   private options: winston.LoggerOptions;
   private meta: any = {};
@@ -23,15 +21,6 @@ export class WinstonLogger implements ILogger {
 
   public pop(key: string) {
     delete this.meta[key];
-  }
-
-  public getLoggingContext(): ILoggingContext {
-    return {
-      requestId: this.context.requestId,
-      userId: this.context.jwt?.sub ?? '<anonymous>',
-      scopes: this.context.scopes,
-      ...this.meta,
-    };
   }
 
   public trace(message: string, options?: LoggerOptions) {
@@ -55,6 +44,6 @@ export class WinstonLogger implements ILogger {
   }
 
   private output(message: string, logFn: (message: string, ...args: any[]) => void, options?: LoggerOptions) {
-    logFn(message, { ...options?.extras, ...this.getLoggingContext() });
+    logFn(message, { ...options?.extras, ...this.context.loggingContext, ...this.meta });
   }
 }
