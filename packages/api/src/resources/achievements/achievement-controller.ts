911e944f816211579a2a051fc3e9c4e6bc2a2b8b
Niclas Lindstedt <nicl@slindstedt.se>
2022-04-26 21:22:22 +0200

    chore: fix unleash deployment

--- packages/api/src/resources/achievements/achievement-controller.ts
+++ packages/api/src/resources/achievements/achievement-controller.ts
@@ -160,9 +160,6 @@ export class AchievementController {
     const paginationOpts = getPaginationOptions(req);
     validateOrderBy(paginationOpts.orderBy, AchievementMapper.isProperty);
 
-    const ctx = getContext();
-    ctx.logger.push('hello', 'world');
-
     const service = new GetAchievementService(getContext());
     const achievements = await service.getMany(paginationOpts);
 
