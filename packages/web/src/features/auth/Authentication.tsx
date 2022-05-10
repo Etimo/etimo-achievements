266ebddefce89dc0d76fb4a74392e863dfe9bef9
Niclas Lindstedt <niclaslindstedt@users.noreply.github.com>
2022-05-10 10:45:08 +0200

    Update Authentication.tsx

--- packages/web/src/features/auth/Authentication.tsx
+++ packages/web/src/features/auth/Authentication.tsx
@@ -38,7 +38,7 @@ const Authentication: React.FC = ({ children }) => {
   const [loginState, setLoginState] = useState<LoginState>();
 
   /**
-   * If we think we're logged in, validate our login cookie.
+   * If local storage indicates we might be logged in, set that state.
    */
   useEffect(() => {
     if (isLoggedIn()) {
