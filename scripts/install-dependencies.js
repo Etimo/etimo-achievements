ed3dc6fc5761b8790f9820d4cd09585bf4c5cf41
Niclas Lindstedt <nicl@slindstedt.se>
2022-05-13 07:43:07 +0200

    chore: remove global dependencies

--- scripts/install-dependencies.js
+++
@@ -1,5 +0,0 @@
-import installDependencies from './utils/install-dependencies.js';
-
-const wantedDependencies = ['jest', 'knex', 'ts-node', 'typescript:tsc', 'webpack-cli:webpack'];
-
-installDependencies(wantedDependencies);
