a567cbcc104edfa8c975bb803d020f3ebee28372
Axel Elmarsson <axel.elmarsson@gmail.com>
2022-08-02 16:44:43 +0200

    feat: achievement description in give award view

--- packages/web/src/components/form/FormSelect.tsx
+++ packages/web/src/components/form/FormSelect.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select';
 import CardRow from '../cards/CardRow';
 
 type Props = {
@@ -6,7 +7,7 @@ type Props = {
   text?: string;
   bindValue: any;
   onChange: (value: any) => void;
-  options: { value: string; label: string }[];
+  options?: { value: string; label: string; subtitle?: string }[];
 };
 
 const FormSelect: React.FC<Props> = ({ label, text, bindValue, onChange, options }) => {
@@ -14,22 +15,26 @@ const FormSelect: React.FC<Props> = ({ label, text, bindValue, onChange, options
     <CardRow label={label}>
       <div className="w-full">
         <div className="relative">
-          <select
-            className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            value={bindValue}
-            onChange={(e) => {
-              onChange(e.target.value);
+          <Select
+            placeholder={text}
+            className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            options={options}
+            noOptionsMessage={() => 'No achievements'}
+            components={{
+              DropdownIndicator: undefined,
+              Option: ({ data, setValue }) => {
+                return (
+                  <MultilineOption
+                    value={data.value}
+                    title={data.label}
+                    subtitle={data.subtitle}
+                    onClick={(value: string) => setValue({ value, label: data.label }, 'select-option')}
+                  />
+                );
+              },
             }}
-          >
-            <option key="" value="">
-              {text ?? 'Select a value'}
-            </option>
-            {options?.map((child) => (
-              <option key={child.value} value={child.value}>
-                {child.label}
-              </option>
-            ))}
-          </select>
+            onChange={(value) => onChange(value?.value)}
+          />
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
@@ -38,6 +43,22 @@ const FormSelect: React.FC<Props> = ({ label, text, bindValue, onChange, options
         </div>
       </div>
     </CardRow>
+  );
+};
+
+interface MultilineOptionProps {
+  value: string;
+  title: string;
+  subtitle?: string;
+  onClick: (value: string) => void;
+}
+
+const MultilineOption = ({ value, title, subtitle, onClick }: MultilineOptionProps) => {
+  return (
+    <div className="hover:bg-blue-300 py-1 px-2 cursor-pointer" onClick={() => onClick(value)}>
+      <div className="font-medium">{title}</div>
+      <div className="text-xs text-gray-500 font-thin">{subtitle}</div>
+    </div>
   );
 };
 
