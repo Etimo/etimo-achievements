f1afa9ddf0eee9ec84c47de834d6cefc8b9a0d8e
Axel Elmarsson <axel.elmarsson@gmail.com>
2022-08-02 14:20:30 +0200

    feat: add support for cell tooltips

--- packages/web/src/components/table/TableCell.tsx
+++ packages/web/src/components/table/TableCell.tsx
@@ -1,16 +1,25 @@
+import { Tooltip } from '@mui/material';
 import React from 'react';
 import { mergeClasses } from '../../common/utils/merge-classes';
 
 type Props = {
   className?: string;
+  tooltip?: string;
 };
 
-const TableCell: React.FC<Props> = ({ className, children, ...rest }) => {
-  return (
+const TableCell: React.FC<Props> = ({ className, children, tooltip, ...rest }) => {
+  const content = (
     <td className={mergeClasses('p-3 border border-slate-300', className)} {...rest}>
       {children}
     </td>
   );
+
+  // Wrap cell in tooltip
+  if (tooltip) {
+    return <Tooltip title={tooltip}>{content}</Tooltip>;
+  }
+
+  return content;
 };
 
 export default TableCell;
