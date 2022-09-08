362bd845a7ce122df98550fbca0f4e07b48ae2a5
Axel Elmarsson <33149910+elmaxe@users.noreply.github.com>
2022-09-08 15:46:55 +0200

    fix: filtering (#390)
    * fix: filtering
    
    * fix: remove console log

--- packages/types/src/pagination-options.ts
+++ packages/types/src/pagination-options.ts
@@ -2,7 +2,7 @@ export type PaginationOptions = {
   skip: number;
   take: number;
   orderBy: OrderByOption[];
-  filters?: Record<string, any>;
+  where?: Record<string, any>;
 };
 
 export type OrderByOption = [key: string, order: 'asc' | 'desc'];
