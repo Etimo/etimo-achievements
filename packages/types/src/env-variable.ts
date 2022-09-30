dbe4c94b0d1a01b20c13e8e2321a9ba4fc1881dd
Axel Elmarsson <axel.elmarsson@gmail.com>
2022-09-30 16:02:11 +0200

    fix: remove duplicate env variable

--- packages/types/src/env-variable.ts
+++ packages/types/src/env-variable.ts
@@ -25,7 +25,6 @@ export type EnvVariable =
   | 'SLACK_CHANNEL_MEDIUM'
   | 'SLACK_TEAM_ID'
   | 'SLACK_TOKEN'
-  | 'ENABLE_SLACK_MESSAGES'
   | 'UNLEASH_TOKEN'
   | 'UNLEASH_URL'
   | 'NOTIFY_SLACK';
