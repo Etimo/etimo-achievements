8a63ad608419c323649a700581e771daf64c308c
Niclas Lindstedt <niclaslindstedt@users.noreply.github.com>
2023-02-17 13:53:48 +0100

    Update 06-clients.ts

--- packages/data/seeds/06-clients.ts
+++ packages/data/seeds/06-clients.ts
@@ -2,20 +2,20 @@ import * as Knex from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
   // Inserts seed entries
-  await knex('seasons').insert([
+  await knex('clients').insert([
     {
       id: '89bbe48f-9465-44fa-822c-b7d927bcd854',
       clientSecret: '$2b$10$UoZV/qxSkEkIb7cKYOCbiuJUDIcpS7s/VhekUtrkcNmreVdby0tpO',
-      name: 'A client',
-      description: 'A client',
+      name: 'a-client',
+      description: "Niclas' client that can read awards and achievements",
       userId: 'a11ea55b-9a6c-4f50-a390-b787f3dc2ad5',
       scope: 'r:awards r:achievements',
     },
     {
       id: 'd7a93903-8fd1-49c7-85cc-0cbe948889d8',
       clientSecret: '$2b$10$UoZV/qxSkEkIb7cKYOCbiuJUDIcpS7s/VhekUtrkcNmreVdby0tpO',
-      name: 'A client',
-      description: 'A client',
+      name: 'a-client',
+      description: "Axel's client that can read awards and achievements",
       userId: '537574f5-605d-491e-bd23-108e373aa8ca',
       scope: 'r:awards r:achievements',
     },
